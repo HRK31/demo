6b9c7e3964b02fab3155b8c2c89e0f76ab4c43a8
HRK31 <hrk10321@gmail.com>
2020-08-15 17:12:49 +0530

    Defined onSelect() option in the sidebar.component.ts

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  @Output() selected = new EventEmitter<string> ();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(value: string){
+    this.selected.emit(value);
+  }
 }
