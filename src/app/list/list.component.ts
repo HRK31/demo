cc43c3e2395b221184cf7a1555756858473e9810
HRK31 <hrk10321@gmail.com>
2020-08-16 19:15:55 +0530

    Created an array of string that consists of three objects

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  label: string[]=['Label','Label','Label'];
   constructor() { }
 
   ngOnInit(): void {
